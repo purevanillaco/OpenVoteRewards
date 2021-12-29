76a40f41b2d448dbdb806b2389f73ce2d24a55a7
quiquelhappy <quiquelhappy@gmail.com>
2021-12-29 13:44:30 +0100

    spigot support

---
+++ src/main/java/co/purevanilla/mcplugins/openvoterewards/spigot/OpenVoteRewards.java
@@ -0,0 +1,11 @@
+package co.purevanilla.mcplugins.openvoterewards.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OpenVoteRewards extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}

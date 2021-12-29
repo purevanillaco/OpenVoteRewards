fb2a7068fc382c7ae1d7edb973e0e8cb5e82b5b0
quiquelhappy <quiquelhappy@gmail.com>
2021-12-29 16:58:10 +0100

    quantified item with fixed or random quantities

---
+++ src/main/java/co/purevanilla/mcplugins/openvoterewards/api/reward/quantity/QuantifiedItem.java
@@ -0,0 +1,10 @@
+package co.purevanilla.mcplugins.openvoterewards.api.reward.quantity;
+
+import org.bukkit.inventory.ItemStack;
+
+public class QuantifiedItem {
+
+    Quantity quantity;
+    ItemStack itemStack;
+
+}

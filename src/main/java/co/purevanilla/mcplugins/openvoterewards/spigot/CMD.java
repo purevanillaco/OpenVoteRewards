3ac06612d68a29db9d6073e2555054a8d050c7c4
quiquelhappy <quiquelhappy@gmail.com>
2021-12-29 13:44:44 +0100

    cmd handler

---
+++ src/main/java/co/purevanilla/mcplugins/openvoterewards/spigot/CMD.java
@@ -0,0 +1,14 @@
+package co.purevanilla.mcplugins.openvoterewards.spigot;
+
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
+
+@CommandAlias("vote")
+public class CMD {
+
+    @Default()
+    public void onVoteCMD(){
+
+    }
+
+}
